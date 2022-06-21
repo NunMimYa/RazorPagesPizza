95e3e452ed742f1d06e53666c728f1fbad5e0cd5
NunMimYa <ljezequelpfl@hotmail.fr>
2022-06-21 20:54:28 +0200

    First commit

---
+++ Pages/Pizza.cshtml.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesPizza.Models;
+using RazorPagesPizza.Services;
+
+
+namespace RazorPagesPizza.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        public void OnGet()
+        {
+            pizzas = PizzaService.GetAll();
+        }
+
+        public List<Pizza> pizzas = new();
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            PizzaService.Add(NewPizza);
+            return RedirectToAction("Get");
+        }
+
+        public string GlutenFreeText(Pizza pizza)
+        {
+            if (pizza.IsGlutenFree)
+                return "Gluten Free";
+            return "Not Gluten Free";
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            PizzaService.Delete(id);
+            return RedirectToAction("Get");
+        }
+            
+        [BindProperty]
+        public Pizza NewPizza { get; set; }        
+    }
+}
